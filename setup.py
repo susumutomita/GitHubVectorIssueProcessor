53217b8d9732d903ab870bf404accade2e0c768a
bull <oyster880@gmail.com>
2024-07-11 01:53:12 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+
+with open("requirements.txt", "r") as f:
+    required_packages = f.read().splitlines()
 
 setup(
     name="GitHubVectorIssueProcessor",
     version="1.0.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    install_requires=[
-        "Flask==1.1.2",
-        "flask-cors==3.0.10",
-        "psycopg2-binary==2.8.6",
-        "openai==0.5.0",
-        "qdrant-client==0.9.0",
-        "python-dotenv==0.15.0",
-        "regex==2021.4.4",
-        "PyGithub==1.55",
+    version="0.1",
+    packages=[
+        "src",
     ],
+    install_requires=required_packages,
     entry_points={
         "console_scripts": [
             "github-vector-issue-processor=main:main",
