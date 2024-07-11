c8a7cb13a57855c4eaf3fe73d05d1702fc794169
bull <oyster880@gmail.com>
2024-07-11 23:59:22 +0000

    fix install error

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name="GitHubVectorIssueProcessor",
     version="1.0.0",
+    packages=[
+        "app",
+    ],
     entry_points={
         "console_scripts": [
             "github-vector-issue-processor=app.main:main",
